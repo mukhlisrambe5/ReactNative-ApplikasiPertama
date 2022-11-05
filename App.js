c887bb756080a7324ff5554ff2d0e4b584e95471
mukhlisrambe5 <mukhlis.rambe5@gmail.com>
2022-11-06 06:35:13 +0700

    ubah informasi default welcome RN

--- App.js
+++ App.js
@@ -76,18 +76,8 @@ const App: () => Node = () => {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-           <Text>Hello World</Text>
+           <Text>Hello World ..</Text>
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
         </View>
       </ScrollView>
     </SafeAreaView>
